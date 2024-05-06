79e6fb97e7d949f3f9b6d7726e8c8b0308d9cda4
xduck7 <okgj5@yandex.ru>
2024-05-06 22:34:08 +0300

    jumpSearch

--- search/jumpSearch/jumpSearch.go
+++ search/jumpSearch/jumpSearch.go
@@ -1,3 +1,22 @@
 package jumpSearch
 
-// TODO: algorithm
+import (
+	"math"
+)
+
+func jumpSearch(sortedList []int, target int) bool {
+	step := int(math.Sqrt(float64(len(sortedList))))
+	length := len(sortedList)
+	start := 0
+	end := step
+	for end < length && sortedList[end-1] < target {
+		start = end
+		end += step
+	}
+	for i := start; i < end-1; i++ {
+		if sortedList[i] == target {
+			return true
+		}
+	}
+	return false
+}
