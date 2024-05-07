312ac4659ed263deac6b99ac305c7760875887d4
xduck7 <okgj5@yandex.ru>
2024-05-08 00:15:36 +0300

    FibonacciSearch

--- search/fibonacciSearch/fibonacciSearch.go
+++ search/fibonacciSearch/fibonacciSearch.go
@@ -1,3 +1,42 @@
 package fibonacciSearch
 
-// TODO: algorithm
+func fibonacciSearch(sortedArray []int, target int) bool {
+	length := len(sortedArray)
+	if length == 0 {
+		return false
+	}
+
+	f0 := 0
+	f1 := 1
+	f2 := f0 + f1
+
+	for f2 < length {
+		f0 = f1
+		f1 = f2
+		f2 = f0 + f1
+	}
+
+	offset := -1
+
+	for f1 > 1 {
+		i := min(offset+f0, length-1)
+		if sortedArray[i] < target {
+			f2 = f1
+			f1 = f0
+			f0 = f2 - f1
+			offset = i
+		} else if sortedArray[i] > target {
+			f2 = f0
+			f1 = f1 - f0
+			f0 = f2 - f1
+		} else {
+			return true
+		}
+	}
+
+	if f1 == 1 && sortedArray[offset+1] == target {
+		return true
+	}
+
+	return false
+}
