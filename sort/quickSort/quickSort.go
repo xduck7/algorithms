74dab933ee1c8af79bb0ed915fb96bcc89cf2496
Spl3tnikk <okgj5@yandex.ru>
2024-05-13 00:32:07 +0300

    fix quickSort

--- sort/quickSort/quickSort.go
+++ sort/quickSort/quickSort.go
@@ -2,27 +2,23 @@ package quickSort
 
 func quickSort(array []int) []int {
 	if len(array) <= 1 {
-		return nil
+		return array
 	}
 
-	mid := sort(array)
+	pivot := array[0]
+	var less []int
+	var greater []int
 
-	quickSort(array[:mid])
-	quickSort(array[mid+1:])
-	return array
-}
-
-func sort(array []int) int {
-	pivot := array[len(array)-1]
-	i := -1
-
-	for j := 0; j < len(array)-1; j++ {
-		if array[j] < pivot {
-			i++
-			array[i], array[j] = array[j], array[i]
+	for i := 1; i < len(array); i++ {
+		if array[i] < pivot {
+			less = append(less, array[i])
+		} else {
+			greater = append(greater, array[i])
 		}
 	}
 
-	array[i+1], array[len(array)-1] = array[len(array)-1], array[i+1]
-	return i + 1
+	less = quickSort(less)
+	greater = quickSort(greater)
+
+	return append(append(less, pivot), greater...)
 }
